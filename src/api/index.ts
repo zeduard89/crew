b92571c66d0aa9f3d2c5fe8799cb2abb111f68e4
zeduard89 <zeduard89@gmail.com>
2023-07-03 04:04:28 -0300

    fix: deplouy

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// const URL = 'http://localhost:3001'
-const URL = import.meta.env.VITE_CREWDB_URL ?? 'http://localhost:3001'
+const URL = 'https://backcrew-production.up.railway.app'
+// const URL = import.meta.env.VITE_CREWDB_URL ?? 'http://localhost:3001'
 export const CrewApi = axios.create({
   baseURL: URL,
   headers: {
