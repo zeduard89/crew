5b236a42a77b4159c68b15223bc4feec26fff305
Kleguiz <kevin_dev27@outlook.com>
2023-07-03 15:13:59 -0300

    profile contributions merge

---
+++ src/interfaces/Payments.ts
@@ -0,0 +1,12 @@
+export interface IPayments {
+    id: string
+    userId: string
+    status: string
+    email: string
+    orderId: number
+    description: string
+    firstName: string
+    transactionAmount: number
+    dateApproved: string
+    ordertype: string
+}
