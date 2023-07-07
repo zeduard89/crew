f817b36f7b6c2f80869f4bda42e79aebe5bc69a9
zeduard89 <zeduard89@gmail.com>
2023-07-07 18:35:59 -0300

    comment: ProjectData.ts, se comento la info harcodeada

--- src/data/ProjectData.ts
+++ src/data/ProjectData.ts
@@ -103,77 +103,77 @@ export const mainCarrouselProjects: MainCarrouselProject[] = [
   },
 ]
 
-export const projectsDetail: IProject[] = [
-  {
-    id: '1',
-    title: 'Project 1',
-    mainImage: 'https://picsum.photos/500/400',
-    shortDescription:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Nulla euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl.',
-    fundingCurrent: 1000,
-    fundingPercentage: 50,
-    fundingDayLeft: 30,
-    category: 'Tech & Innovation',
-    images: [
-      'https://picsum.photos/500/400',
-      'https://picsum.photos/400/500',
-      'https://picsum.photos/500/500',
-      'https://picsum.photos/500/600',
-    ],
-    fundingGoal: 2000,
-    fundingGoalReached: false,
-    creatorId: '1',
-    updates: [],
-  },
-  {
-    id: '2',
-    title: 'Project 2',
-    mainImage: 'https://picsum.photos/500/500',
-    shortDescription:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Nulla euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl.',
-    fundingCurrent: 654,
-    fundingPercentage: 13,
-    fundingDayLeft: 30,
-    category: 'Tech & Innovation',
-    images: [
-      'https://picsum.photos/500/400',
-      'https://picsum.photos/400/500',
-      'https://picsum.photos/500/500',
-      'https://picsum.photos/500/600',
-    ],
-    fundingGoal: 2000,
-    fundingGoalReached: false,
-    creatorId: '1',
-    updates: [],
-  },
-  {
-    id: '3',
-    title: 'Project 3',
-    mainImage: 'https://picsum.photos/500/600',
-    shortDescription:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Nulla euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl.',
-    fundingCurrent: 10000,
-    fundingPercentage: 100,
-    fundingDayLeft: 0,
-    category: 'Tech & Innovation',
-    images: [
-      'https://picsum.photos/500/400',
-      'https://picsum.photos/400/500',
-      'https://picsum.photos/500/500',
-      'https://picsum.photos/500/600',
-    ],
-    fundingGoal: 10000,
-    fundingGoalReached: true,
-    creatorId: '1',
-    updates: [],
-  },
-]
+// export const projectsDetail: IProject[] = [
+//   {
+//     id: '1',
+//     title: 'Project 1',
+//     mainImage: 'https://picsum.photos/500/400',
+//     shortDescription:
+//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Nulla euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl.',
+//     fundingCurrent: 1000,
+//     fundingPercentage: 50,
+//     fundingDayLeft: 30,
+//     category: 'Tech & Innovation',
+//     images: [
+//       'https://picsum.photos/500/400',
+//       'https://picsum.photos/400/500',
+//       'https://picsum.photos/500/500',
+//       'https://picsum.photos/500/600',
+//     ],
+//     fundingGoal: 2000,
+//     fundingGoalReached: false,
+//     creatorId: '1',
+//     updates: [],
+//   },
+//   {
+//     id: '2',
+//     title: 'Project 2',
+//     mainImage: 'https://picsum.photos/500/500',
+//     shortDescription:
+//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Nulla euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl.',
+//     fundingCurrent: 654,
+//     fundingPercentage: 13,
+//     fundingDayLeft: 30,
+//     category: 'Tech & Innovation',
+//     images: [
+//       'https://picsum.photos/500/400',
+//       'https://picsum.photos/400/500',
+//       'https://picsum.photos/500/500',
+//       'https://picsum.photos/500/600',
+//     ],
+//     fundingGoal: 2000,
+//     fundingGoalReached: false,
+//     creatorId: '1',
+//     updates: [],
+//   },
+//   {
+//     id: '3',
+//     title: 'Project 3',
+//     mainImage: 'https://picsum.photos/500/600',
+//     shortDescription:
+//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Nulla euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Donec euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl.',
+//     fundingCurrent: 10000,
+//     fundingPercentage: 100,
+//     fundingDayLeft: 0,
+//     category: 'Tech & Innovation',
+//     images: [
+//       'https://picsum.photos/500/400',
+//       'https://picsum.photos/400/500',
+//       'https://picsum.photos/500/500',
+//       'https://picsum.photos/500/600',
+//     ],
+//     fundingGoal: 10000,
+//     fundingGoalReached: true,
+//     creatorId: '1',
+//     updates: [],
+//   },
+// ]
 
 export const images: string[] = [
   'https://picsum.photos/500/400',
